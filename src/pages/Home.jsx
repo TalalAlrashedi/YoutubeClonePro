c2c3ce170e855442f1daa7ac10c88414d0a3d8e9
TalalAlrashedi <TalalAlrashedi24@hotmail.com>
2025-06-04 08:46:56 +0300

    upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -61,7 +61,7 @@ const Home = () => {
     <div className="bg-[#121212] min-h-screen text-white pt-16">
       <Navbar onSearch={handleSearch} />
 
-      <div className="max-w-7xl mx-auto p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {videos.map((video) => {
           const videoId = video.id.videoId || video.id;
           const snippet = video.snippet;
